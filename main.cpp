b1df67ad1f79ac63edc92c97093d4ee0b3d8d6c7
Altoid <152133261+AItoid@users.noreply.github.com>
2024-10-27 18:50:54 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,27 +1,27 @@
-#include "include.h"
+#include "ptr.h"
 
-struct ExampleStruct
+struct test_t
 {
-    int someNumber;
-    const char* someText;
+    int num;
+    const char* text;
 };
 
 int main()
 {
     // Create encrypted pointer.
-    ExampleStruct* exampleStruct = reinterpret_cast<ExampleStruct*>(g_ptr->Encrypt(new ExampleStruct()));
+    test_t* test = g_crypt->enc<test_t*>(new test_t());
 
     // Print pointer.
-    printf("Encrypted: 0x%p\n", exampleStruct);
-    printf("Decrypted: 0x%p\n", reinterpret_cast<ExampleStruct*>(g_ptr->Decrypt(exampleStruct)));
+    printf("Encrypted: 0x%p\n", test);
+    printf("Decrypted: 0x%p\n", g_crypt->dec<test_t*>(test));
 
     // Set pointer data.
-    reinterpret_cast<ExampleStruct*>(g_ptr->Decrypt(exampleStruct))->someNumber = 100;
-    reinterpret_cast<ExampleStruct*>(g_ptr->Decrypt(exampleStruct))->someText = "RandomText";
+    g_crypt->dec<test_t*>(test)->num = 1337;
+    g_crypt->dec<test_t*>(test)->text = "RandomText";
 
     // Print pointer members.
-    printf("Num: %i\n", reinterpret_cast<ExampleStruct*>(g_ptr->Decrypt(exampleStruct))->someNumber);
-    printf("Text: %s\n", reinterpret_cast<ExampleStruct*>(g_ptr->Decrypt(exampleStruct))->someText);
+    printf("Num: %i\n", g_crypt->dec<test_t*>(test)->num);
+    printf("Text: %s\n", g_crypt->dec<test_t*>(test)->text);
 
     std::cin.get();
     return 0;
