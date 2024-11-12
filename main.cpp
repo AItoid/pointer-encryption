60095237cc87d5c0fd7fbacfec24fca5e3090d56
Altoid <152133261+AItoid@users.noreply.github.com>
2024-11-11 17:24:03 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,33 +2,22 @@
 
 struct test_t
 {
-    int num;
-    const char* text;
+	int num;
 };
 
 int main()
 {
-    // Create encrypted pointer.
-    test_t* test = g_ptr->process<test_t*>(new test_t(), false);
+	test_t* prot = reinterpret_cast<test_t*>(g_ptr->process<test_t*>(new test_t()));
+	printf("Encrypted: 0x%p\n", prot);
+	printf("Decrypted: 0x%p\n", g_ptr->process<test_t*>(prot));
 
-    // Print pointer.
-    printf("Encrypted: 0x%p\n", test);
-    printf("Decrypted: 0x%p\n", g_ptr->process<test_t*>(test, true));
+	for (int i = 0; i < 100; i++)
+	{
+		g_ptr->process<test_t*>(prot)->num++;
+		printf("%i\n", g_ptr->process<test_t*>(prot)->num);
+		Sleep(500);
+	}
 
-    // Set pointer data.
-    g_ptr->process<test_t*>(test, true)->num = 1;
-    g_ptr->process<test_t*>(test, true)->text = "RandomText";
-
-    // Print pointer members.
-    printf("Num: %i\n", g_ptr->process<test_t*>(test, true)->num);
-    printf("Text: %s\n", g_ptr->process<test_t*>(test, true)->text);
-
-    for (int i = 0; i < 100; i++)
-    {
-        PROCESS_PTR_AUTO(test, true)->num++;
-        printf("%i\n", PROCESS_PTR_AUTO(test, true)->num);
-    }
-
-    std::cin.get();
-    return 0;
+	std::cin.get();
+	return 0;
 }
